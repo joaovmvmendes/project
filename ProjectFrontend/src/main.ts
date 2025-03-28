2149ea98d379332cbbc65c46436572200b4174e3
joaovmvmendes <joao.vmvmendes@gmail.com>
2025-03-28 00:07:18 -0300

    Guards

--- ProjectFrontend/src/main.ts
+++ ProjectFrontend/src/main.ts
@@ -3,15 +3,14 @@ import { provideRouter, Route } from '@angular/router';
 import { LoginComponent } from './app/features/auth/components/login/login.component';
 import { TaskListComponent } from './app/features/task/pages/task-list/task-list.component';
 import { AppComponent } from './app/app.component';
+import { Routes } from '@angular/router';
 
-const routes: Route[] = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/login' },
   { path: 'tasks', component: TaskListComponent },
 ];
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-  ],
+  providers: [provideRouter(routes)],
 });
